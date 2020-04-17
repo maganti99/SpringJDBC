098b53e1e9b6607142dd1b5b80476ebe33e799c9
Nagesh Maganti <nagesh.maganti@macys.com>
2020-04-17 11:27:06 -0400

    Added Java Config file and aaditional methods in JDBC Impl class

---
+++ src/main/java/com/nagesh/jdbc/config/Config.java
@@ -0,0 +1,24 @@
+package com.nagesh.jdbc.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import javax.sql.DataSource;
+
+@Configuration
+@ComponentScan(value={"com.nagesh.jdbc.dao"})
+public class Config {
+			
+		@Bean
+		public DataSource getDataSource(){
+			System.out.println("Bean DriverManagerDataSource created");
+			DriverManagerDataSource driverManagerDataSource = new DriverManagerDataSource();
+			driverManagerDataSource.setDriverClassName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+			driverManagerDataSource.setUrl("jdbc:sqlserver://L4754344:1433;DatabaseName=nagdb;integratedSecurity=true");
+			driverManagerDataSource.setUsername("");
+			driverManagerDataSource.setPassword("");
+			return driverManagerDataSource ;
+		}
+}
--- src/main/java/com/nagesh/jdbc/test/runJdbcTemp.java
+++ src/main/java/com/nagesh/jdbc/test/runJdbcTemp.java
@@ -4,9 +4,11 @@ package com.nagesh.jdbc.test;
 
 import java.util.List;
 
-import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+//import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import com.nagesh.jdbc.config.Config;
 import com.nagesh.jdbc.dao.EmplDAOImpl;
 import com.nagesh.jdbc.model.Empl;
 
@@ -14,9 +16,11 @@ public class runJdbcTemp {
 
 	public static void main(String[] args) {
 		
+		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(Config.class);
 		
-		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
-				"applicationContext.xml");
+		//XML Config
+		/*ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
+				"applicationContext.xml");*/
 		EmplDAOImpl emplDAOImpl = context.getBean(EmplDAOImpl.class);
 		
 		List<Empl> empList = emplDAOImpl.getAll();
@@ -27,6 +31,25 @@ public class runJdbcTemp {
 		}
 		
 		context.close();
+		
+		Empl emp1 = emplDAOImpl.getById2(2, 3000);
+		System.out.println(emp1.getName() + " " + emp1.getDept() + " " + emp1.getSal());
+		
+		
+		System.out.println("count :" + emplDAOImpl.getCount());
+		
+		Empl e = new Empl();
+		e.setId((short)5);
+		e.setName("Harish");
+		e.setDept("Fin");
+		e.setSal(6700);
+		        
+		emplDAOImpl.save(e); 
+				         
+		
+		
+					
+		
 
 	}
 
